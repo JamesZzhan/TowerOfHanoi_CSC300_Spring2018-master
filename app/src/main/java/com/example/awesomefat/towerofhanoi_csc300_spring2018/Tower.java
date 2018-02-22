480602fbfbca775c1fa04fabba5ce6a85abaca4b
JamesZzhan <33325455+JamesZzhan@users.noreply.github.com>
2018-02-22 00:53:10 -0600

    Initial commit

---
+++ app/src/main/java/com/example/awesomefat/towerofhanoi_csc300_spring2018/Tower.java
@@ -0,0 +1,51 @@
+package com.example.awesomefat.towerofhanoi_csc300_spring2018;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Tower
+{
+    private Disk top;
+    private ViewGroup towerVisual;
+
+    public Tower(ViewGroup towerVisual)
+    {
+        this.top = null;
+        this.towerVisual = towerVisual;
+    }
+
+    public void push(Disk d)
+    {
+        //equivalent to add front for linked lists
+        if(this.top == null)
+        {
+            this.top = d;
+        }
+        else
+        {
+            d.setNextDisk(this.top);
+            this.top = d;
+        }
+        this.towerVisual.addView(d.getDiskVisual(), 0);
+    }
+
+    public Disk peek()
+    {
+        return this.top;
+    }
+
+    public Disk pop()
+    {
+        Disk disk2Remove = this.top;
+
+        if(disk2Remove != null)
+        {
+            this.top = disk2Remove.getNextDisk();
+            disk2Remove.setNextDisk(null);
+            this.towerVisual.removeViewAt(0);
+        }
+        return disk2Remove;
+    }
+}
